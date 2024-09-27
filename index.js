84f6c54001ebac32c4429ab1402f22577491ffd2
Matheus da silva <matheussilvainter1234@gmail.com>
2024-09-26 21:43:19 -0300

    aprendendo alguns comandos de javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+//alert("Olha o JS aqui!")
+/*
+Esse é meu primeiro script JS
+Estou aprendendo sobre como comentar o código
+*/
+
+let nome=prompt('Qual o seu nome?')
+alert('seja bem vindo ' + nome + '!')
+
+console.log('imprimindo no console do navegador')
+
+
+
